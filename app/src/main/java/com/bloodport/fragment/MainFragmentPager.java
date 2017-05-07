f1d4e4977b86a0b06b03299121ee1d0a372dd097
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2017-05-07 23:27:27 +0500

    Implementing suitable tab for ViewPager

--- app/src/main/java/com/bloodport/fragment/MainFragmentPager.java
+++ app/src/main/java/com/bloodport/fragment/MainFragmentPager.java
@@ -1,8 +1,11 @@
 package com.bloodport.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +25,14 @@ public class MainFragmentPager extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_main_fragment_pager, container, false);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+
         ViewPager vpPager = (ViewPager) view.findViewById(R.id.vpPager);
         vpPager.setAdapter(new PagerAdapter(getActivity().getSupportFragmentManager()));
+
+        tabLayout.setupWithViewPager(vpPager);
+
         return view;
     }
 }
